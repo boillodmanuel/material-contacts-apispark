381eb32860c1efb871cfea310d9122e70ea2b569
anqitong <none@none.com>
2015-10-27 15:42:11 +0100

    add junit tests

---
+++ src/main/java/net/apispark/webapi/db/ContactPersistenceTest.java
@@ -0,0 +1,39 @@
+package net.apispark.webapi.db;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import net.apispark.webapi.representation.Contact;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ContactPersistenceTest extends ContactPersistence {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		Assert.assertThat("test is good", is(instanceOf(String.class)));
+	}
+	
+	@Test
+	public void returnContact() {
+		//Contact c =ContactPersistence.INSTANCE.addContact(null);
+		Contact c = new Contact("123", "Contact1", "Lastname", "No avatar");
+		ContactPersistence.INSTANCE.addContact(c);
+		Assert.assertThat(c, is(instanceOf(Contact.class)));
+	}
+	
+	@Test
+	public void getContact() {
+		//ContactPersistence c =ContactPersistence.INSTANCE.addContact(null);
+		//String id = c.getId();
+		//Assert.assertThat(id, is(instanceOf(String.class)));
+		Contact c = new Contact("123", "Contact1", "Lastname", "No avatar");
+		Contact c1 = ContactPersistence.INSTANCE.getContact("123");
+		Assert.assertThat(c1, is(instanceOf(Contact.class)));
+	}
+	
+	
+
+}
